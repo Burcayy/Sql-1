bc3d2dc08c800229309c209029a27e2fbe02f08e
muratvuranok <muratvuranok@hotmail.com>
2019-10-06 14:01:23 +0300

    Ders1 sonu

---
+++ Lesson1.sql
@@ -0,0 +1,24 @@
+﻿-- Yorum satırı
+
+CREATE DATABASE SecondDB; --  Database Olusturma
+GO                        -- yukaridaki islem bitmeden alt satira gecme
+
+USE SecondDB;
+GO
+
+CREATE TABLE Categories ( 
+             CategoryId   INT PRIMARY KEY IDENTITY(1 , 1) , 
+             CategoryName NVARCHAR(50) NOT NULL , -- bos gecilemez olarak isaretledik
+             Description  NVARCHAR(150) NULL ,    -- default olarak bos gecilebilir yazmasaniz bi le, bos gecilebilir olacaktir 
+             CreatedDate  DATETIME DEFAULT(GETDATE())
+                        );  
+GO
+
+CREATE TABLE Products ( 
+             ProductId    INT PRIMARY KEY IDENTITY(1 , 1) , 
+             ProductName  NVARCHAR(50) NOT NULL , 
+             UnitsInStock SMALLINT DEFAULT(1) , 
+             UnitPrice    MONEY , 
+             CreatedDate  DATETIME DEFAULT(GETDATE()) , 
+             CategoryId   INT FOREIGN KEY REFERENCES Categories(CategoryId) -- bir kategorinin birden fazla ürünü olur, ürünler tablosuna foreignkey olarak kategori tablosunun primary key alanını ekliyoruz.
+                      );
